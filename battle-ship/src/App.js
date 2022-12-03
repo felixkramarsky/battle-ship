4e137644c387f9081e804a381baf3f1caa3867b8
c23fk <c23fk@dalton.org>
2022-12-03 14:15:11 -0500

    interactivity

--- battle-ship/src/App.js
+++ battle-ship/src/App.js
@@ -14,13 +14,21 @@ function Square(props) {
 }
 
 class Game extends React.Component {
-  handleClick(x,y){
+  handleClick(i,j){
+    const replaceBoard = this.state.player1Turn ? this.state.player1Board: this.state.player2Board;
+    replaceBoard[i][j]++;
+    this.setState({
+      player1Board: this.state.player1Turn ? replaceBoard: this.state.player1Board,
+      player2Board: this.state.player1Turn ? this.state.player2Board: replaceBoard,
+      player1Turn: !this.state.player1Turn
+    })
     return;
   }
 
   constructor(props){
     let size = 7
-    const newBoard = [...new Array(size).keys()].map((i) => [...new Array(size).keys()].map((j) => (i*7)+j));
+    //const newBoard = [...new Array(size).keys()].map((i) => [...new Array(size).keys()].map((j) => (i*7)+j));
+    const newBoard = new Array(size).fill(0).map((i) => new Array(size).fill(0));
     super(props);
     this.state = {
         player1Board: JSON.parse(JSON.stringify(newBoard)),
@@ -30,7 +38,7 @@ class Game extends React.Component {
   }
   render(){
     return (
-      <Board currentBoard = {this.state.player1Turn ? this.state.player1Board: this.state.player2Board} onClick = {(x,y) => this.handleClick(x,y)} />
+      <Board player1 = {this.state.player1Turn} currentBoard = {this.state.player1Turn ? this.state.player1Board: this.state.player2Board} onClick = {(x,y) => this.handleClick(x,y)} />
     )
   }
 }
@@ -40,25 +48,28 @@ class Board extends React.Component {
     return (
       <Square
         value={this.props.currentBoard[i][j]}
-        onClick={() => this.props.onClick(i)}
+        onClick={() => this.props.onClick(i,j)}
       />
     );
   }
   render() {
     return (
-      <div>    
-      {
-        this.props.currentBoard.map((row, i) => {
-          let index = i;
-          return(
-            <div className = "board-row">
-              {this.props.currentBoard[0].map((square, j) => {
-                return this.renderSquare(index, j);
-              })}
-            </div>
-          )
-          })
-      }
+      <div>
+        {this.props.player1? <h1>Player 1</h1>: <h1>Player 2</h1>}    
+        <div>
+        {
+          this.props.currentBoard.map((row, i) => {
+            let index = i;
+            return(
+              <div className = "board-row">
+                {this.props.currentBoard[0].map((square, j) => {
+                  return this.renderSquare(index, j);
+                })}
+              </div>
+            )
+            })
+        }
+        </div>
       </div>
     );
   }
