c1f4c79a11d3c4b6b9bc098d797020d98a299c27
c23fk <c23fk@dalton.org>
2022-12-13 11:19:05 -0500

    added two boards per player

--- battle-ship/src/App.js
+++ battle-ship/src/App.js
@@ -29,9 +29,19 @@ class Game extends React.Component {
     return;
   }
 
+  genEnemyBoard(board){
+    const newBoard = JSON.parse(JSON.stringify(board));
+    for(let i = 0; i < size; i++){
+      for(let j = 0; j < size; j++){
+        if(newBoard[i][j].ship && !newBoard[i][j].hit){
+          newBoard[i][j].ship = false;
+        }
+      }
+    }
+    return newBoard;
+  }
+
   constructor(props){
-    
-    //const newBoard = [...new Array(size).keys()].map((i) => [...new Array(size).keys()].map((j) => (i*7)+j));
     const newBoard = new Array(size).fill(0).map((i) => new Array(size).fill({ship: false, hit: false}));
     super(props);
     this.state = {
@@ -42,23 +52,42 @@ class Game extends React.Component {
   }
   render(){
     return (
-      //<Board size = {size} player1 = {this.state.player1Turn} currentBoard = {this.state.player1Turn ? this.state.player1Board: this.state.player2Board} onClick = {(x,y) => this.handleClick(x,y)} />
-      <div className="game">
-      <Board size = {size} player1 = {true} currentBoard = {this.state.player1Board} onClick = {(x,y) => this.handleClick(x,y)} />
-      <hr/>
-      <Board size = {size} player1 = {false} currentBoard = {this.state.player2Board} onClick = {(x,y) => this.handleClick(x,y)} />
+      <div>
+        {this.state.player1Turn? <h1>Player 1</h1>: <h1>Player 2</h1>} 
+        <Board clickable = {true} size = {size} board = "enemy" currentBoard = {this.state.player1Turn ? this.genEnemyBoard(this.state.player1Board): this.genEnemyBoard(this.state.player2Board)} onClick = {(x,y) => this.handleClick(x,y)} />
+        <Board clickable = {false} size = {size} board = "me" currentBoard = {this.state.player1Turn ? this.state.player2Board: this.state.player1Board} onClick = {(x,y) => this.handleClick(x,y)} />
       </div>
+
+      // <div className="game">
+      // <Board size = {size} player1 = {true} currentBoard = {this.state.player1Board} onClick = {(x,y) => this.handleClick(x,y)} />
+      // <hr/>
+      // <Board size = {size} player1 = {false} currentBoard = {this.state.player2Board} onClick = {(x,y) => this.handleClick(x,y)} />
+      // </div>
     )
   }
 }
 
 class Board extends React.Component {
+
+  getValue(square){
+    if(square.hit){
+      if(square.ship){
+        return "X";
+      }
+      return "O";
+    }
+    if(square.ship){
+      return "S";
+    }
+    return null;
+  }
+
   renderSquare(i,j) {
     return (
       <Square
         key = {i*this.props.size+j}
-        value={this.props.currentBoard[i][j].hit?"X":null}
-        onClick={() => this.props.onClick(i,j)}
+        value={this.getValue(this.props.currentBoard[i][j])}
+        onClick={this.props.clickable?() => this.props.onClick(i,j):null}
       />
     );
   }
@@ -66,7 +95,7 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        {this.props.player1? <h1>Player 1</h1>: <h1>Player 2</h1>}    
+        {this.props.board === "enemy"? <h2>Enemy Board</h2>: <h2>Allied Board</h2>}    
         <div>
         {
           this.props.currentBoard.map((row, i) => {
